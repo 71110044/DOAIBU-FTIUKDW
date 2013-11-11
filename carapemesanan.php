0fdaf8a8abe57b82d249a0de525a23202e87ed60
71110044 <71110044@students.ukdw.ac.id>
2013-11-11 22:28:19 +0700

    upload...

---
+++ carapemesanan.php
@@ -0,0 +1,30 @@
+			<div id ="carapesan">
+				<h3>Cara Pemesanan</h3>
+				<p>
+					<ol>
+						<li>Pilihlah barang sesuai keinginan anda yang ada di website kami. Barang yang kami sediakan merupakan barang yang tersedia.</li>
+						<li>Anda diharuskan melakukan login terlebih dahulu sebelum pembelian.</li>
+						<li>Ikuti proses pemesanan dan pengiriman.</li>
+						<li>Setelah melakukan transfer, harap mengisi form konfirmasi pembayaran.</li>
+						<li>Admin akan memvalidasi transaksi anda dan barang akan dikirim.</li>
+					</ol>
+				</p>
+				<p id="satu">
+				Dalam pemesanan barang tidak terdapat minimum order, kami melayani pembelian eceran maupun grosir. Pemesanan dapat dilakukan setiap saat 7 hari 24 jam, kami akan merespon cepat setiap pesanan yang masuk ke kami .  Untuk pembelian bisa melakukan pemesanan untuk respon cepat ke :
+				</p>
+				<p id="dua"><br/>
+				<strong>Herlius Caraka Dewata</strong><br/><br/>
+				SMS : 0812345678<br/><br/>
+				BBM: 22AABBCC <br/>
+				</p>
+				<p id="tiga"><br/>
+    			Pemesanan melalui sms/bbm dengan format : <br/>
+    			<ul>
+				<li>Nama, Alamat Lengkap dan No. Telp/Hp</li>
+    			<li>Nama Produk</li>
+    			<li>Jumlah yang akan dipesan</li>
+    			</ul>
+				<strong>Contoh: Martin, Jln. manaajaboyeh, Yogyakarta, 08982404xxxx, Arsenal, 1.</strong><br/><br/>
+				Kami akan mengkonfirmasikan kepada anda mengenai status produk ada/ tidaknya, total harga, dan nomor rekening tujuan untuk transfer (BCA dan Mandiri).<br/>
+				</p>
+			</div>
